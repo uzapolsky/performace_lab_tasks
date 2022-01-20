a154bfc7b8a650fb9aaee092814320023d581d09
uzapolsky <uzapolsky@gmail.com>
2022-01-20 18:44:39 +0300

    let coords be float

--- task2/task2.py
+++ task2/task2.py
@@ -1,25 +1,26 @@
 import argparse
+import math
 
 
 def in_square_check(cx, cy, dx, dy, r):
     sq_length = (dx - cx) ** 2 + (dy - cy) ** 2
     sq_rad = r ** 2
-    if sq_length > sq_rad:
-        return 2
+    if math.isclose(sq_length, sq_rad, rel_tol=1e-12):
+        return 0
     elif sq_length < sq_rad:
         return 1
-    return 0
+    return 2
 
 
 def main(file1, file2):
 
     with open(file1, 'r') as f1:
-        center_x, center_y = map(int, f1.readline().split())
-        radius = int(f1.readline())
+        center_x, center_y = map(float, f1.readline().split())
+        radius = float(f1.readline())
 
     with open(file2, 'r') as f2:
         for dot in f2.readlines():
-            dot_x, dot_y = map(int, dot.split())
+            dot_x, dot_y = map(float, dot.split())
             print(in_square_check(center_x, center_y, dot_x, dot_y, radius))
 
 
